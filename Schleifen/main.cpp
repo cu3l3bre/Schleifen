ae1e07a035e19c98950a5081381b275c0f181d9d
cu3l3bre <cu3l3bre@googlemail.com>
2019-05-03 11:04:39 +0200

    for, while, do-while

--- Schleifen/main.cpp
+++ Schleifen/main.cpp
@@ -4,6 +4,48 @@ using namespace std;
 
 int main()
 {
+	cout << "Programm zu Schleifen" << endl;
+
+	// for-Schleife
+	for (int i=0;i<5;i++)
+	{
+		cout << i << endl;
+	}
+
+	
+	// while-Schleife
+	// um sich die do-while zu sparen, einfach die pruefung so init, dass die bedingung true ist
+	char Benutzereingabe = 'j';
+
+	//cout << "Wollen Sie die Schleife sehen? [j,n]: ";
+	//cin >> Benutzereingabe;
+	
+	while (Benutzereingabe == 'j')
+	{
+		cout << "while-Schleife" << endl;
+
+		cout << "Wollen Sie die Schleife nochmal sehen? [j,n]: ";
+		cin >> Benutzereingabe;
+	}
+
+
+	int j = 1;
+	while (j <= 10)
+	{
+		cout << "Zahl: " << j << endl;
+		j++;
+	}
+
+
+	// do-while Schleife
+	do
+	{
+		cout << "Das ist eine do-while-Schleife" << endl;
+		cout << "Wollen Sie die do-while-Schleife nochmal sehen?[j,n]: ";
+		cin >> Benutzereingabe;
+	}
+	while (Benutzereingabe == 'j');
+
 
 	system("pause");
 	return 0;
